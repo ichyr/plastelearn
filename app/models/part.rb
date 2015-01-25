3b91a66a7b24544708fd5c59d879234ccddc876c
ichyr <ichyr@softserveinc.com>
2015-01-25 20:12:45 +0200

    sticky footer incorporated

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,4 +1,10 @@
 class Part < ActiveRecord::Base
 	belongs_to :course
 	has_many :homeworks
+
+	after_initialize :set_default_status, :if => :new_record?
+
+  def set_default_status
+    self.status ||= PART_STATUSES[:ACTIVE]
+  end
 end
