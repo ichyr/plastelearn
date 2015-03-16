120fea30e54a353c9c7c2fb31fc1e98f8e1cb197
ichyr <ichyr@softserveinc.com>
2015-03-16 02:33:30 +0200

    deleted obsolete file

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   root to: 'courses#index'
   
-  devise_for :users, controllers: { registrations: 'registrations'}
+  devise_for :users
   
   resources :comments
 
