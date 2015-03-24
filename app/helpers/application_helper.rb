0fcba0c5a288afa22ff112392e53bd81a8544389
ichyr <ichyr@softserveinc.com>
2015-03-24 13:27:23 +0200

    added date-time to commets view. changed email to username

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
 			"No date specified"
 		end
 	end
+
+	def format_date_comments date
+		if date 
+			date.strftime "%m %B of %Y at %H:%M"
+		else 
+			"No date specified"
+		end
+	end
 end
