fc35b2d381ab330197ff821f3bb0d813699f247b
ichyr <ichyr@softserveinc.com>
2015-04-11 00:17:11 +0300

    altered user creation services

--- app/services/create_user_service.rb
+++ app/services/create_user_service.rb
@@ -1,8 +1,8 @@
 class CreateUserService
   def call email
     user = User.find_or_create_by!(email: email) do |user|
-        user.password = Rails.application.secrets.admin_password
-        user.password_confirmation = Rails.application.secrets.admin_password
+        user.password = Rails.application.secrets.admin_password || "changeme"
+        user.password_confirmation = Rails.application.secrets.admin_password || "changeme"
       end
   end
 end
