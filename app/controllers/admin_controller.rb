c815bbe85c180ac734c0e4abe447592b77ed39f4
ichyr <ichyr@softserveinc.com>
2015-06-08 03:02:58 +0300

    added search to Admin#courses

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,6 +4,8 @@ class AdminController < ApplicationController
 
   after_action :verify_authorized
 
+  respond_to :html
+
   def users_index
     authorize :admin
     
@@ -19,9 +21,11 @@ class AdminController < ApplicationController
   def courses
     authorize :admin
 
-  	pages = Course.count / PER_PAGE
+  	pages = (Course.count / PER_PAGE) + 1
   	current_page = params[:page].to_i > pages ? pages : params[:page]
+    search = "%#{params[:search]}%"
 
-  	@courses = Course.paginate(:page => current_page, :per_page => PER_PAGE)
+  	@courses = Course.where('title like ?', search)
+                     .paginate(:page => current_page, :per_page => PER_PAGE)
   end
 end
