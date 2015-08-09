4e56e1487024739c0b709b42b670053ab4851090
ichyr <zmii@plast.org.ua>
2015-08-09 17:11:04 +0300

    added course#description view. added defaults

--- app/models/course.rb
+++ app/models/course.rb
@@ -8,6 +8,8 @@ class Course < ActiveRecord::Base
 	has_many :registries, dependent: :destroy
   has_many :users, through: :registries
 
+  after_create :set_defaults, :if => :new_record?
+
   # validates :title, length: { in: 5..30 }
   # validates :enrollment_key, length: { minimum: 8 }
   # validates :short_description, length: { in: 10..255 }
@@ -17,4 +19,9 @@ class Course < ActiveRecord::Base
   def self.search(search)
     where('title LIKE ?', "%#{search}%")
 	end
+
+  private
+  def set_defaults
+    self.documentation = "Please edit this in the course administration page!"
+  end
 end
